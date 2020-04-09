8d16b701cb3997f5043ca2a3890272c45dd4b4f8
Justin Bessert <63407135+bessertja@users.noreply.github.com>
2020-04-09 09:39:38 -0400

    Added project starter files

---
+++ quotes.js
@@ -0,0 +1,35 @@
+let quotes = [
+    {
+        quote: "Change is. Growth is optional.",
+        source: "James A. Bessert",
+        tag: "personal growth"
+    },
+    {
+        quote: ". . .ask not what your country can do for you - ask what you can do for your country.",
+        source: "President John F. Kennedy",
+        citation: "Inaugural Address, Washington D.C.",
+        year: 1961,
+        tag: "government"
+    },
+    {
+        quote: "I have a dream today.",
+        source: "Martin Luther King, Jr.",
+        citation: "I Have A Dream, Lincoln Memorial, Washington D.C.",
+        year: 1963,
+        tag: "civil rights"
+    },
+    {
+        quote: "This above all: to thine own self be true",
+        source: "William Shakespeare",
+        citation: "Hamlet, act I, scene iii, line 78",
+        year: 1600,
+        tag: "personal growth"
+    },
+    {
+        quote: "If you desire many things, many things will seem but a few.",
+        source: "Benjamin Franklin",
+        citation: "Poor Richard's Almanack",
+        year: 1736,
+        tag: "wealth"
+    },
+];
